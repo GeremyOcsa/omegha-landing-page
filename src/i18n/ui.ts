17383680593a169e25d1be7573b0f0828fb91594
GeremyOcsa <geremy.ocsa11@gmail.com>
2025-03-02 01:45:12 -0500

    Traduccion y utils

---
+++ src/i18n/ui.ts
@@ -0,0 +1,178 @@
+export const languageList = {
+  es: "Español",
+  en: "English",
+};
+
+export const labels = {
+  es: {
+    //Nav
+    "nav.home": "Inicio",
+    "nav.about": "Nosotros",
+    "nav.enginnering": "Ingenieria",
+    "nav.projects": "Proyectos",
+    "nav.service": "Servicio",
+    "nav.contact": "Contactenos",
+    // Inicio
+    "index.title": "Sean Bienvenidos",
+    "index.firstP":
+      "En <strong>OMEGHA CONSTRUCTOR'S BUILDING Co. S.A.C.</strong> nos distinguimos por la calidad de la mano de obra e insumos utilizados en cada una de nuestras obras, lo que nos permite garantizar una entrega puntual y elevados estándares de calidad, a un precio justo.",
+    "index.secondP":
+      "Destacamos también por la búsqueda constante de nuevas e innovadoras soluciones de ingeniería y diseño arquitectónico, ecológicas y amigables con el medio ambiente, rompiendo todos los estándares establecidos.",
+    // Nosotros
+    "about.title": "Nuestra Empresa",
+    "about.firstP":
+      "<strong>Omegha Constructor Building Co. S.A.C.</strong> es una empresa orientada a la industria de la construcción, desarrollando consultorías y proyectos de todo el ámbito de la ingeniería y la arquitectura, así como la construcción de obras civiles, obras de saneamiento, obras electromecánicas, puentes y carreteras, y servicios de construcción de edificación e inmobiliarios.",
+    "about.secondP":
+      "También nos dedicamos al mantenimiento de infraestructura, administración de mantenimiento de edificios, saneamiento legal de Inmuebles; y brindamos soluciones informáticas para el e-Marketing y el negocio electrónico.",
+    "mission.title": "Mision",
+    "mission.des":
+      "Basándonos en las necesidades de infraestructura y de espacios y/o instalaciones productivas, ofrecer soluciones ingenieriles y/o inmobiliarias de alta calidad mediante la continua innovación en el diseño, una búsqueda permanente de nuevas y mejores tecnologías, el perfeccionamiento de los procesos constructivos y el servicio personalizado a nuestros clientes.",
+    "vision.title": "Vision",
+    "vision.des":
+      "Consolidarnos como la mayor solución de calidad ingenieril de construcción en el oriente peruano, contribuyendo al desarrollo de la región generando valor con nuestras obras y proyectos.",
+    "values.title": "Nuestros Valores",
+    "values.des":
+      "En <strong>Omegha Constructor Building Co. S.A.C.</strong> partimos de dos principios fundamentales:",
+    "values.liF": "El Respeto por el medio en que se interviene, y",
+    "values.liP": "El Respeto por las etnias que habitan el lugar.",
+    "values.para":
+      "Nuestra empresa desarrolla proyectos amigables con la naturaleza, respetando sus riquezas, biodiversidad, paisajes, microclimas y las actividades productivas de los pobladores lugareños.",
+    "values.firstI":
+      "Asimismo, respetamos en nuestros proyectos, las costumbres, el folklore y las manifestaciones culturales de la población lugareña.",
+    "values.secondI":
+      "Es de esta forma que nuestras soluciones ingenieriles y constructivas armonizan con el medio sin dañarlo o modificarlo sensiblemente, e impulsan el crecimiento sostenible, ordenado y permanente de las actividades productivas y económicas de la población. Mejorando su calidad de vida, potenciando su productividad, y generando valor.",
+    "values.firstS":
+      "Nos debemos a nuestro producto, y ello nos compromete a buscar permanentemente la excelencia en todos nuestros quehaceres y servicios.",
+    "values.secondS": "Al interior de nuestra organización,",
+    "values.thirdS":
+      "Son nuestra impecable constante y se conjugan para obtener nuestro mejor producto al servicio de usted.",
+    // Ingenieria
+    "engi.title": "Ingenieria",
+    "engi.des":
+      "Nuestro enfoque por ofrecer siempre la máxima calidad y la satisfacción de nuestros clientes es el motor que nos impulsa a mantenernos a la vanguardia de las nuevas tecnologías, en una búsqueda constante de soluciones de ingeniería eficientes, ecológicas, adaptables al medio ambiente y económicas.",
+    "engi.span": "Algunos ejemplos de nuestra ingeniería y mas:",
+    "firstPro.title": "Diseño 3D del Condominio Biosostenible Yori",
+    "firstPro.des":
+      "Construido en una zona de amortiguamiento su serio compromiso con el entorno, la flora y la fauna del lugar hacen de este un muy rico ejemplo de convivencia humano-ambiental. Las soluciones constructivas y materiales empleados han sido elegidos y desarrollados para lograr la sostenibilidad del proyecto y su adaptación al medio ambiente.",
+    "secondPro.title": "Diseño 3D de Iglesia en Tarapoto",
+    "secondPro.des":
+      "Mediante la incorporación de nuevas tecnologías y conceptos se logra el rediseño y mejoramiento de edificaciones y espacios antiguos, con diseños innovadores y futuristas que modernizan y enriquecen su arquitectura y que otorguen valor y protagonismo de los mismos en la región, al mismo tiempo que se preserva el atractivo cultural e histórico de la población lugareña.",
+    "thirdPro.title": "Diseño de estructuras y armaduras especiales",
+    "thridPro.des":
+      "En este campo la ingeniería de detalle es determinante, explotando al máximo las bondades del material y cuidando los limites del mismo. Conocer la naturaleza intima de la materia y su versatilidad permite crear aleaciones eficientes para soluciones estructurales de gran demanda. Ejemplo de esto es la armadura especial diseñada para la entrada principal del Hospital de Tarapoto.",
+    "fourthPro.title":
+      "Primera carretera en el Perú construida bajo el sistema de petrificación de suelos",
+    "fourthPro.des":
+      "Tras ensayos y pruebas encontramos la solución mas viable para el 90% del suelo peruano, la estabilización con copolímeros, una tecnología de muy alta eficiencia y rendimiento, aprobada por el MTC, que aglomera partículas finas en un proceso irreversible de petrificación, lo que dota de mucha mayor resistencia al suelo.",
+    // Proyectos
+    "pro.title": "Proyectos",
+    "pro.des":
+      "Contamos con un equipo de colaboradores profesionales, tecnología apropiada, maquinaria, equipo y la organización necesaria para realizar de manera eficiente obras de diversa envergadura. Nuestros diseños e ingeniería de vanguardia enfocados en la máxima calidad, siempre cuidando el medio ambiente y evitando los sobre costos, son las cualidades que incrementan el valor agregado de nuestros proyectos.",
+    "pro.span": "Algunos de nuestros proyectos más destacados:",
+    // Servicios
+    "serv.title": "Consultoría y Construcción",
+    "serv.h2": "Servicios de arquitectura, urbanismo y diseño",
+    "serv.des":
+      "Ofrecemos servicios de elaboración y diseño de planos arquitectónicos para viviendas unifamiliares, multifamiliares y para toda clase de edificaciones residenciales y/o comerciales. Manejamos un programa tridimensional para generar vistas en 3d que usted vea cómo quedará el diseño antes de su construcción, logrando así un diseño personalizado y un desarrollo del proyecto integral ajustándonos a las necesidades del cliente y a su presupuesto.",
+    "serv.para1":
+      "Diseño de un centro Comercial de 3 plantas proyectado para un terreno con pendiente de inclinación",
+    "serv.para2":
+      "Diseño y modelado en 3D de pistas pavimentadas para la Nueva Vía de Evitamiento de Huancayo en el Perú",
+    "serv.para3":
+      "Diseño y modelado en 3D para un Centro de Acogida Residencial",
+    // Contacto
+    "contact.title": "Contactenos",
+    "contact.p":
+      "Para más información sobre nuestros servicios y/o proyectos, póngase en contacto con nosotros y lo atenderemos personalmente.",
+    "contact.write": "Escribanos en:",
+    "contact.location": "Ubiquenos en:",
+    "contact.tarapoto": "Oficina Tarapoto",
+    "contact.lima": "Oficina Lima",
+    "contact.web": "Nuestra web:",
+  },
+  en: {
+    //Nav
+    "nav.home": "Home",
+    "nav.about": "About us",
+    "nav.enginnering": "Engineering",
+    "nav.projects": "Projects",
+    "nav.service": "Service",
+    "nav.contact": "Contact us",
+    // Inicio
+    "index.title": "Be Welcome",
+    "index.firstP":
+      "In <strong>OMEGHA CONSTRUCTOR'S BUILDING Co. S.A.C.</strong> we distinguish ourselves by the quality of the workmanship and supplies used in each of our projects and services, which allows us to guarantee timely delivery and high-quality standards, at a fair price.",
+    "index.secondP":
+      "We also stand out for the constant search for new and innovative engineering and architectural design solutions, ecological and friendly to the environment, breaking all established standards.",
+    // Nosotros
+    "about.title": "Our Company",
+    "about.firstP":
+      "<strong>Omegha Constructor Building Co. S.A.C.</strong> is a company oriented to the development of architectural design projects, civil works, sanitation, and electromechanical, and construction services and real estate. In addition to design, consultancy and sale of furniture and office equipment.",
+    "about.secondP":
+      "We are also dedicated to maintaining infrastructure, building maintenance management, legal consolidation of property, and provide solutions for e-Marketing and e-business.",
+    "mission.title": "Mission",
+    "mission.des":
+      "Based on the needs of infrastructure and spaces and or production facilities, we offer providing engineering and or real estate of high quality through the continuous innovation in the design, a permanent search of new and better technologies, the improvement of construction processes and personalized service to our customers.",
+    "vision.title": "Vision",
+    "vision.des":
+      "As the largest consolidate solution engineering quality of construction in the middle of Peru, contributing to the development of the region by creating value with our works and projects.",
+    "values.title": "Our Values",
+    "values.des":
+      "In <strong>Omegha Constructor's Building Co. S.A.C</strong> we start from two fundamental principles:",
+    "values.liF": "Respect for the environment in which it intervenes, and",
+    "values.liP": "Respect for the ethnic groups that inhabit the place",
+    "values.para":
+      "Our company develops projects friendly with nature, respecting their wealth, biodiversity, landscapes, microclimates and the productive activities of the Settlers locals.",
+    "values.firstI":
+      "We respect in our projects, customs, folklore and cultural manifestations of the villager population.",
+    "values.secondI":
+      "It is in this way that our engineering solutions and constructive harmonize with the middle without damaging it or modify it considerably, and spur sustainable growth, orderly and permanent productive and economic activities of the population. Improving their quality of life, enhancing their productivity and generating value.",
+    "values.firstS":
+      "We owe it to our product, and it commits us to permanently seek excellence in all our chores and services.",
+    "values.secondS": "Within our Organization,",
+    "values.thirdS":
+      "Are our impeccable constant and combine to give our best product to service you.",
+    // Ingenieria
+    "engi.title": "Engineering",
+    "engi.des":
+      "Our focus on always offering the highest quality and customer satisfaction is the driving force that keeps us at the forefront of new technologies, in a constant search for efficient, ecological, environmentally friendly and economical engineering solutions.",
+    "engi.span": "Some examples of our engineering and more:",
+    "firstPro.title": "3D Design of the Yori Biosustainable Condominium",
+    "firstPro.des":
+      "Built in a buffer zone, its serious commitment to the environment, flora and fauna of the place make this a very rich example of human-environmental coexistence. The construction solutions and materials used have been chosen and developed to achieve the sustainability of the project and its adaptation to the environment.",
+    "secondPro.title": "3D Design of Church in Tarapoto",
+    "secondPro.des":
+      "Through the incorporation of new technologies and concepts, the redesign and improvement of old buildings and spaces is achieved, with innovative and futuristic designs that modernize and enrich their architecture and give them value and prominence in the region, while preserving the cultural and historical attractiveness of the local population.",
+    "thirdPro.title": "Design of special structures and reinforcement",
+    "thridPro.des":
+      "In this field, detailed engineering is decisive, making the most of the material's advantages and taking care of its limits. Knowing the intimate nature of the material and its versatility allows the creation of efficient alloys for high demand structural solutions. An example of this is the special reinforcement designed for the main entrance of the Tarapoto Hospital.",
+    "fourthPro.title":
+      "First highway in Peru built under the soil petrification system",
+    "fourthPro.des":
+      "After trials and tests we found the most viable solution for 90% of the Peruvian soil, stabilization with copolymers, a technology of very high efficiency and performance, approved by the MTC, which agglomerates fine particles in an irreversible process of petrification, which gives much greater resistance to the soil.",
+    // Proyectos
+    "pro.title": "Projects",
+    "pro.des":
+      "We have a team of professional collaborators, appropriate technology, machinery, equipment and the necessary organization to efficiently carry out works of diverse scope. Our state-of-the-art designs and engineering focused on maximum quality, always taking care of the environment and avoiding cost overruns, are the qualities that increase the added value of our projects.",
+    "pro.span": "Some of our most outstanding projects:",
+    // Servicios
+    "serv.title": "Consulting and construction",
+    "serv.h2": "Architecture and design services",
+    "serv.des":
+      "We offer services for the preparation and design of architectural plans for single-family and multi-family homes and for all types of residential and/or commercial buildings. We use a three-dimensional program to generate 3D views that you see will be the design before its construction and thus make modifications before its execution, thus achieving a personalized design and a comprehensive project development adjusting to the client's needs and their budget.",
+    "serv.para1":
+      "Design of a 3-storey shopping center planned for a sloping site",
+    "serv.para2":
+      "Design and 3D modeling of paved roadways for the New Huancayo Evitamiento Road in Peru",
+    "serv.para3": "3D design and modeling for a Residential Care Center",
+    // Contacto
+    "contact.title": "Contact Us",
+    "contact.p":
+      "For more information about our services and/or projects, please contact us and we will attend you personally.",
+    "contact.write": "Write to us at:",
+    "contact.location": "Locate us at:",
+    "contact.tarapoto": "Tarapoto Office",
+    "contact.lima": "Lima Office",
+    "contact.web": "Our website:",
+  },
+};
