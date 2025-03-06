72e7d8dcd65d2c9b80570f6b06a5aa1082c18935
GeremyOcsa <geremy.ocsa11@gmail.com>
2025-03-05 20:56:51 -0500

    Nuevo componente para el Slider

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,41 @@
+import React, { useRef, useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import img1 from "../assets/slider/inicio001.webp";
+import img2 from "../assets/slider/inicio002.jpg";
+import img3 from "../assets/slider/inicio003.jpg";
+import img4 from "../assets/slider/inicio004.jpg";
+import img5 from "../assets/slider/inicio005.jpg";
+
+const images = [img1.src, img2.src, img3.src, img4.src, img5.src];
+
+import { Autoplay } from "swiper/modules";
+
+export default function Carousel() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 6000,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+        className="mySwiper h-full w-full md:animate-slide-left"
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={image}
+              alt="Imagenes Slider"
+              className="h-full w-full rounded-2xl border-2 border-slate-950 object-cover"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
