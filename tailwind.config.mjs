5c383e1e6d9a3d26a5c34d63e0a6906abb3236ca
GeremyOcsa <geremy.ocsa11@gmail.com>
2025-03-06 14:26:05 -0500

    Animaciones para desktop y mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -21,16 +21,52 @@ export default {
       },
       keyframes: {
         "slide-right": {
-          "0%": { opacity: "0", transform: "translateX(-150px)" },
-          "100%": { opacity: "1", transform: "translateX(0px)" },
+          "0%": { opacity: "0", transform: "translateX(-35px)" },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0px)",
+            visibility: "visible",
+          },
         },
         "slide-left": {
-          "0%": { opacity: "0", transform: "translateX(150px)" },
-          "100%": { opacity: "1", transform: "translateX(0px)" },
+          "0%": { opacity: "0", transform: "translateX(35px)" },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0px)",
+            visibility: "visible",
+          },
         },
         "slide-top": {
+          "0%": { opacity: "0", transform: "translateY(35px)" },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0px)",
+            visibility: "visible",
+          },
+        },
+        "slide-right-desktop": {
+          "0%": { opacity: "0", transform: "translateX(-150px)" },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0px)",
+            visibility: "visible",
+          },
+        },
+        "slide-left-desktop": {
+          "0%": { opacity: "0", transform: "translateX(150px)" },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0px)",
+            visibility: "visible",
+          },
+        },
+        "slide-top-desktop": {
           "0%": { opacity: "0", transform: "translateY(150px)" },
-          "100%": { opacity: "1", transform: "translateY(0px)" },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0px)",
+            visibility: "visible",
+          },
         },
       },
       animation: {
@@ -38,6 +74,11 @@ export default {
         "slide-right-slow": "slide-right 1.25s ease-out forwards",
         "slide-left": "slide-left 1s ease-out forwards",
         "slide-top": "slide-top 1s ease-out forwards",
+
+        // Versión para Desktop
+        "slide-right-desktop": "slide-right-desktop 0.75s ease-out forwards",
+        "slide-left-desktop": "slide-left-desktop 1s ease-out forwards",
+        "slide-top-desktop": "slide-top-desktop 1s ease-out forwards",
       },
     },
   },
